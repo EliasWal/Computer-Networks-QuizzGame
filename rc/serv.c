d79c932aad3c44b2bb78ee27fd278ace4958dc70
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-26 01:01:15 +0200

    Update serv.c

--- rc/serv.c
+++ rc/serv.c
@@ -1,230 +1,190 @@
-//gcc -o serv serv.c cJSON/build/libcjson.a -lm -lsqlite3
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <errno.h>
 #include <unistd.h>
-#include <pthread.h>
-#include "cJSON/cJSON.h"
-#include "JSON_Functions.h"
-#include <sqlite3.h>
-#include "SQLite_Functions.h"
-
-#define DB_FILE "questions.db"
-#define PORT 2728
-#define MAXTHREADS 1000000
-#define MAX_Questions 20
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
 
 
 
-int client_fds[MAXTHREADS];
-pthread_t threads[MAXTHREADS];
-pthread_mutex_t mutex;
-pthread_barrier_t barrier;  // Barieră pentru a sincroniza clienții
+/* codul de eroare returnat de anumite apeluri */
+extern int errno;
 
-void remove_newline(char *str) {
-    size_t len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';  // Șterge caracterul nou
+/* portul de conectare la server*/
+int port;
+
+
+
+void registerClient(int sd) {
+  char msg[100];
+
+  /* citirea mesajului */
+  bzero(msg, 100);
+  printf("[client] Introduceti un nume: ");
+  fflush(stdout);
+  read(0, msg, 100);
+  char fullMsg[200];
+  snprintf(fullMsg, sizeof(fullMsg), "nume: %s", msg);
+
+  /* trimiterea mesajului la server */
+  if (write(sd, fullMsg, sizeof(fullMsg)) <= 0)
+  {
+    perror("[client] Eroare la write() spre server.\n");
+    exit(errno);
+  }
+//  /* trimiterea mesajului la server */
+//  if (write(sd, msg, 100) <= 0)
+//  {
+//    perror("[client] Eroare la write() spre server.\n");
+//    exit(errno);
+//  }
+
+  /* citirea raspunsului dat de server (apel blocant pana cand serverul raspunde) */
+  if (read(sd, msg, 100) < 0)
+  {
+    perror("[client] Eroare la read() de la server.\n");
+    exit(errno);
+  }
+
+  /* afisam mesajul primit */
+  printf("[client] Mesajul primit este: %s\n", msg);
+}
+
+void readyOrNot(int sd) {
+  char msg[100];
+
+    /* citirea mesajului */
+    bzero(msg, 100);
+    printf("[client] Raspunde cu DA cand esti gata sa incepi! : ");
+    fflush(stdout);
+    read(0, msg, 100);
+    char fullMsg[200];
+
+    snprintf(fullMsg, sizeof(fullMsg), "ready: %s", msg);
+
+    /* trimiterea mesajului la server */
+    if (write(sd, fullMsg, 100) <= 0)
+    {
+      perror("[client] Eroare la write() spre server.\n");
+      exit(errno);
+    }
+
+    /* citirea raspunsului dat de server (apel blocant pana cand serverul raspunde) */
+//      system("clear");
+    if (read(sd, msg, 100) < 0)
+    {
+      perror("[client] Eroare la read() de la server.\n");
+      exit(errno);
+    }
+
+
+    /* afisam mesajul primit */
+    printf("[client] Mesajul primit este: %s\n", msg);
+
+
+
+}
+
+void answer(int sd) {
+  char msg[100];
+
+
+    while(1){
+
+        /* citirea mesajului */
+        bzero(msg, 100);
+        printf("[client] Raspunde cu a, b, c sau d: ");
+        fflush(stdout);
+        read(0, msg, 100);
+        char fullMsg[200];
+
+        snprintf(fullMsg, sizeof(fullMsg), "raspuns: %s", msg);
+
+        /* trimiterea mesajului la sserver */
+        if (write(sd, fullMsg, 100) <= 0)
+        {
+          perror("[client] Eroare la write() spre server.\n");
+          exit(errno);
+        }
+
+        /* citirea raspunsului dat de server (apel blocant pana cand serverul raspunde) */
+        if (read(sd, msg, 100) < 0)
+        {
+          perror("[client] Eroare la read() de la server.\n");
+          exit(errno);
+        }
+        /* afisam mesajul primit */
+        printf("[client] Mesajul primit este: %s\n", msg);
+
+        if (write(sd, "next", 100) <= 0)
+        {
+          perror("[client] Eroare la write() spre server.\n");
+          exit(errno);
+        }
+
+        if (read(sd, msg, 100) < 0)
+        {
+          perror("[client] Eroare la read() de la server.\n");
+          exit(errno);
+
+        }
+//        system("clear");
+        printf("--------------------------------------------------\n");
+        printf("[client] Mesajul primit este: %s\n", msg);
+
+
+
     }
 }
 
-void* handle_client(void* arg) {
-    int client_fd = *((int*)arg);
-    char buffer[256];
-    int bytes_received;
-//    char rasp_corect[MAX_Questions] = {'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b', 'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b'};
+int main(int argc, char *argv[])
+{
+  int sd;                      // descriptorul de socket
+  struct sockaddr_in server;   // structura folosita pentru conectare
 
-    // Primeste date de la client
-    bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-    if (bytes_received <= 0) {
-        // Eroare sau clientul s-a deconectat
-        printf("Clientul cu descriptorul %d s-a deconectat.\n", client_fd);
+  /* exista toate argumentele in linia de comanda? */
+  if (argc != 3)
+  {
+    printf("[client] Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+    return -1;
+  }
 
-        // Excludere client din vectorul de descriptori
-        pthread_mutex_lock(&mutex);
-        for (int i = 0; i < MAXTHREADS; ++i) {
-            if (client_fds[i] == client_fd) {
-                client_fds[i] = -1;
-                break;
-            }
-        }
-        pthread_mutex_unlock(&mutex);
-//        const char* filename = "clients.json";
-//        removeClientByFd(filename, client_fd);
-//        printf("Client %d: ", client_fd);
-//        printf("Client with fd %d removed from JSON.\n", client_fd);
+  /* stabilim portul */
+  port = atoi(argv[2]);
 
-//        break;  // Iesire din bucla la deconectare
-    } else {
-        // Procesează datele de la client
-        buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
-        printf("De la clientul %d: %s\n", client_fd, buffer);
-        const char *filename = "clients.json";
+  /* cream socketul */
+  if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("[client] Eroare la socket().\n");
+    return errno;
+  }
 
-        addClient(filename, client_fd, buffer);
+  /* umplem structura folosita pentru realizarea conexiunii cu serverul */
+  /* familia socket-ului */
+  server.sin_family = AF_INET;
+  /* adresa IP a serverului */
+  server.sin_addr.s_addr = inet_addr(argv[1]);
+  /* portul de conectare */
+  server.sin_port = htons(port);
 
-        // Trimite un mesaj de la server către client
-        pthread_mutex_lock(&mutex);
-        const char* response = "Te-ai conectat cu succes! Incepem jocul?";
-        send(client_fd, response, strlen(response), 0);
-        pthread_mutex_unlock(&mutex);
-        bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes_received <= 0) {
-            // Eroare sau clientul s-a deconectat
-            printf("Clientul cu descriptorul %d s-a deconectat.\n", client_fd);
-        }
-        else{
-            char response[100];
-            // Procesează răspunsul de la client
-            buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
-            remove_newline(buffer);
-            printf("Clientul %d a raspuns %s\n", client_fd, buffer);
-            if (strcmp(buffer, "DA\0") == 0) {
-            sprintf(response, "Ready to play!\n");
-            printf("%s", response);
-                send(client_fd, response, strlen(response), 0);
-            }
-        }
-    }
+  /* ne conectam la server */
+  if (connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
+  {
+    perror("[client] Eroare la connect().\n");
+    return errno;
+  }
 
-    printf("Clientul %d a inceput jocul.\n", client_fd);
-    int i = 0;
-     while (i < MAX_Questions) {
-        i++;
+  // Register the client with the server
+  registerClient(sd);
 
-        // Trimite întrebarea către client
-        pthread_mutex_lock(&mutex);
-//        char question[256];
-//        sprintf(question, "Intrebarea %d \n Variante de raspuns: \n a)... \n b) ... \n c) ... \n d) ... \n", i);
-        printf("Intrebarea curenta: %d\n", i);
-//        displayQuestions(i);
-        pthread_mutex_lock(&mutex);
+  // Ready or not communication
+  readyOrNot(sd);
+    answer(sd);
+  /* inchidem conexiunea, am terminat */
+  close(sd);
 
-char questionBuffer[4096];  // Adjust the size as needed
-displayQuestions(i, questionBuffer, sizeof(questionBuffer));
-
-printf("Intrebarea curenta: %d\n", i);
-printf("Raspuns corect: %c\n", rasp_corect[i]);
-send(client_fd, questionBuffer, strlen(questionBuffer), 0);
-
-pthread_mutex_unlock(&mutex);
-//        printf("Raspuns corect: %c\n", rasp_corect[i]);
-//        send(client_fd, question, strlen(question), 0);
-//        pthread_mutex_unlock(&mutex);
-
-        // Așteaptă răspunsul de la client
-        bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (bytes_received <= 0) {
-            // Eroare sau clientul s-a deconectat
-            printf("Clientul cu descriptorul %d s-a deconectat.\n", client_fd);
-            break;
-        } else {
-            char response[100];
-            // Procesează răspunsul de la client
-            buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
-            remove_newline(buffer);
-            printf("Clientul %d a raspuns %s la intrebarea %d. \n", client_fd, buffer, i);
-            if (buffer[0] != rasp_corect[i]){
-                sprintf(response,"Raspuns gresit!\n");
-                printf("Raspuns gresit!\n");
-                send(client_fd, response, strlen(response), 0);
-            }
-            else {
-                sprintf(response,"Raspuns corect!\n");
-                printf("Raspuns corect!\n");
-                send(client_fd, response, strlen(response), 0);
-            }
-            // Așteaptă mesajul de la client
-            bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
-            if (bytes_received <= 0) {
-                // Eroare sau clientul s-a deconectat
-                printf("Clientul cu descriptorul %d s-a deconectat.\n", client_fd);
-                break;
-            }
-            buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
-            printf("Mesaj de la client: %s\n", buffer);
-        }
-    }
-}
-
-
-void initialize_server() {
-    int server_fd, client_fd, i;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len;
-
-    // Creare socket pentru server
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("Eroare la crearea socket-ului");
-        exit(EXIT_FAILURE);
-    }
-
-    // Inițializare structura server_addr
-    bzero((char*)&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Legare socket la adresa și port
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Eroare la legarea socket-ului");
-        exit(EXIT_FAILURE);
-    }
-
-    // Ascultare pentru conexiuni
-    listen(server_fd, 5);
-
-    // Inițializare vector de descriptori de clienti
-    pthread_mutex_init(&mutex, NULL);
-    pthread_barrier_init(&barrier, NULL, MAXTHREADS);
-    for (i = 0; i < MAXTHREADS; ++i) {
-        client_fds[i] = -1;
-    }
-
-    printf("Serverul asteapta conexiuni la portul %d...\n", PORT);
-
-    while (1) {
-        // Accepta conexiunea de la un client
-        client_len = sizeof(client_addr);
-        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-        if (client_fd < 0) {
-            perror("Eroare la acceptarea conexiunii");
-            exit(EXIT_FAILURE);
-        }
-
-        // Adauga noul client la vectorul de descriptori
-        pthread_mutex_lock(&mutex);
-        for (i = 0; i < MAXTHREADS; ++i) {
-            if (client_fds[i] == -1) {
-                client_fds[i] = client_fd;
-                pthread_create(&threads[i], NULL, handle_client, (void*)&client_fds[i]);
-                printf("Clientul cu descriptorul %d s-a conectat.\n", client_fd);
-
-                break;
-            }
-        }
-        pthread_mutex_unlock(&mutex);
-    }
-
-    pthread_barrier_destroy(&barrier);
-    pthread_mutex_destroy(&mutex);
-    close(server_fd);
-}
-
-
-
-int main() {
-    const char *filename = "clients.json";
-    deleteFileContent(filename);
-    initializeDatabase();
-
-    initialize_server();
-
-    return 0;
+  return 0;
 }
