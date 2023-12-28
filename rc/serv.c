6796ab4c85c04c0a0ace78dd45ef5d31365c511b
Elias <96151440+EliasWal@users.noreply.github.com>
2023-12-28 22:51:35 +0200

    Update serv.c

--- rc/serv.c
+++ rc/serv.c
@@ -1,6 +1,4 @@
 //gcc -o serv serv.c cJSON/build/libcjson.a -lm -lsqlite3
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,30 +11,70 @@
 #include "JSON_Functions.h"
 #include <sqlite3.h>
 #include "SQLite_Functions.h"
+#include <signal.h>
+
 
 #define DB_FILE "questions.db"
 #define PORT 2728
 #define MAXTHREADS 1000000
 #define MAX_Questions 20
 
-void askQuestions(int client_fd, int i,pthread_mutex_t* mutex){
-char rasp_corect[MAX_Questions] = {'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b', 'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b'};
-
-        // Trimite întrebarea către client
-        pthread_mutex_lock(&mutex);
-        char question[256];
-        sprintf(question, "Intrebarea %d \n Variante de raspuns: \n a)... \n b) ... \n c) ... \n d) ... \n", i);
-        printf("Intrebarea curenta: %d\n", i);
-        printf("Raspuns corect: %c\n", rasp_corect[i]);
-        send(client_fd, question, strlen(question), 0);
-        pthread_mutex_unlock(&mutex);
-
-}
+time_t start_time;
+int time_remaining;
+int time_expired;
 
 int client_fds[MAXTHREADS];
 pthread_t threads[MAXTHREADS];
 pthread_mutex_t mutex;
 pthread_barrier_t barrier;  // Barieră pentru a sincroniza clienții
+
+void handler(int signo) {
+    printf("Timpul a expirat!\n");
+    exit(EXIT_FAILURE);
+}
+
+void askQuestion(int client_fd, int i){
+//    time_remaining = 20;
+//    time_expired = 0;
+//    start_time = time(NULL);
+
+    char rasp_corect[MAX_Questions] = {'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b', 'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b'};
+
+    // Trimite întrebarea către client
+    pthread_mutex_lock(&mutex);
+    char question[256];
+//    sprintf(question, "Intrebarea %d \n Variante de raspuns: \n a)... \n b) ... \n c) ... \n d) ... \n", i);
+    char *rez = displayQuestion(i,1024);
+//    printf("Intrebarea curenta: %d\n", i);
+//    printf("Raspuns corect: %c\n", rasp_corect[i]);
+    send(client_fd, rez, strlen(rez), 0);
+//    send(client_fd, question, strlen(question), 0);
+    pthread_mutex_unlock(&mutex);
+
+}
+
+void updateTimer(int client_fd,pthread_mutex_t* mutex) {
+    while (time_remaining > 0 && !time_expired) {
+        // Actualizează timpul rămas pe client
+        pthread_mutex_lock(mutex);
+        char timer_update[50];
+        printf(timer_update, "\rTimp ramas: %d secunde", time_remaining);
+//        send(client_fd, timer_update, strlen(timer_update), 0);
+        pthread_mutex_unlock(mutex);
+
+        // Așteaptă un moment
+        sleep(1);
+
+        // Scade timpul rămas
+        time_t current_time = time(NULL);
+        time_remaining = 20 - (current_time - start_time);
+        printf("%d", time_remaining);
+
+    }
+
+    // Resetarea flag-ului pentru timp expirat
+    time_expired = 0;
+}
 
 void remove_newline(char *str) {
     size_t len = strlen(str);
@@ -48,7 +86,7 @@ void remove_newline(char *str) {
 void* handle_client(void* arg) {
     int client_fd = *((int*)arg);
     int i=1;
-    char buffer[256];
+    char buffer[1024];
     int bytes_received;
     char rasp_corect[MAX_Questions] = {'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b', 'a', 'b', 'c', 'd', 'a', 'a', 'a', 'c', 'a', 'b'};
     while(1){
@@ -68,14 +106,14 @@ void* handle_client(void* arg) {
                 }
             }
             pthread_mutex_unlock(&mutex);
-    //        const char* filename = "clients.json";
-    //        removeClientByFd(filename, client_fd);
-    //        printf("Client %d: ", client_fd);
-    //        printf("Client with fd %d removed from JSON.\n", client_fd);
-
+//            const char* filename = "clients.json";
+//            removeClientByFd(filename, client_fd);
+//            printf("Client %d: ", client_fd);
+//            printf("Client with fd %d removed from JSON.\n", client_fd);
+//
             break;  // Iesire din bucla la deconectare
-        } else {
-
+        } else
+        {
             // Procesează datele de la client
             buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
             printf("De la clientul %d: %s\n", client_fd, buffer);
@@ -108,43 +146,54 @@ void* handle_client(void* arg) {
                 // Procesează răspunsul de la client
                 buffer[bytes_received] = '\0'; // Adauga terminatorul la sfarsitul datelor primite
                 remove_newline(buffer);
-                printf("Clientul %d a raspuns %s\n", client_fd, buffer);
+
+                printf("Clientul %d a raspuns: %s\n", client_fd, buffer);
                 if (strstr(buffer, "DA") !=NULL) {
-                    sprintf(response, "Ready to play!\n");
-                    printf("%s", response);
-                    send(client_fd, response, strlen(response), 0);
+//                    sprintf(response, "Ready to play!\n");
+//                    printf("%s", response);
+//                    send(client_fd, response, strlen(response), 0);
                     printf("Clientul %d a inceput jocul.\n", client_fd);
-                    askQuestions(client_fd,i,&mutex);
+                    askQuestion(client_fd, i);
+
+//                    updateTimer(client_fd,&mutex);
                 }
             }
             else
             if (strstr(buffer, "raspuns") != NULL) {
-    char response[100];
-    // Căutați începutul șirului "raspuns"
-    const char *start = strstr(buffer, "raspuns: ");
-    if (start != NULL) {
-        // Extrageți litera care urmează după "raspuns"
-        char answer = start[strlen("raspuns: ")];
-        printf("Clientul %d a raspuns cu litera: %c la intrebarea %d. \n", client_fd, answer, i);
+                char response[100];
+                // Căutați începutul șirului "raspuns"
+                const char *start = strstr(buffer, "raspuns: ");
+                if (start != NULL) {
+                    // Extrageți litera care urmează după "raspuns"
+                    char answer = start[strlen("raspuns: ")];
 
-        if (answer != rasp_corect[i]) {
-            sprintf(response, "Raspuns gresit!\n");
-            printf("Raspuns gresit!\n");
-            send(client_fd, response, strlen(response), 0);
-        } else {
-            sprintf(response, "Raspuns corect!\n");
-            printf("Raspuns corect!\n");
-            send(client_fd, response, strlen(response), 0);
-        }
-    } else {
-        printf("Nu s-a gasit inceputul răspunsului.\n");
-    }
-}
+                    printf("Clientul %d a raspuns cu litera '%c' la intrebarea %d. \n", client_fd, answer, i);
+
+                    char* right_answer = getCorrectAnswer(i);
+
+
+                    if (strncmp(&answer, right_answer, 1) != 0) {
+                        sprintf(response, "Raspuns gresit!\n");
+                        printf("Raspuns gresit!\n\n");
+                        printf("--------------------------------------------------\n\n");
+                        send(client_fd, response, strlen(response), 0);
+                    } else {
+                        sprintf(response, "Raspuns corect!\n");
+                        printf("Raspuns corect!\n\n");
+                        printf("--------------------------------------------------\n\n");
+                        send(client_fd, response, strlen(response), 0);
+                    }
+                } else {
+                    printf("Nu s-a gasit inceputul răspunsului.\n");
+                }
+            }
 
             else
             if (strstr(buffer, "next") != NULL){
                 i++;
-                askQuestions(client_fd,i,&mutex);
+askQuestion(client_fd, i);
+
+//                updateTimer(client_fd,&mutex);
             }
         }
     }
@@ -219,9 +268,12 @@ void initialize_server() {
 
 int main() {
     const char *filename = "clients.json";
-    deleteFileContent(filename);
+//    deleteFileContent(filename);
     initializeDatabase();
-
+//    addQuestions();
+//    deleteDatabaseContent();
+//    resetAutoIncrement();
+    displayAllQuestions(5024);
     initialize_server();
 
     return 0;
